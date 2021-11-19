693a52e67c300dc879b88d5a184de8caa5982476
skillforce <skillforce@mail.ru>
2021-11-19 12:48:46 +0300

    finally commit

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const users = require('./routes/users')
 const app = express()
 const port = 7542;
 
-
-mongoose.connect('mongodb://localhost:27017/users')
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', function () {
-
-})
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost:27017/users')
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error: '));
+// db.once('open', function () {
+//
+// })
 
 
 
@@ -23,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-app.use('/users', users)
+// app.use('/users', users)
 
 app.get('/tasks', (req, res) => {
     res.send('tasks')
@@ -34,8 +33,7 @@ app.use((req, res) => {
 })
 
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT, () => {
 })
 
 
